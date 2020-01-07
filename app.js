e0716a1bd7c445dd9dd8f1af64b28c6a2e96bc75
victorclee <victorchristopherlee@gmail.com>
2020-01-07 13:14:29 -0600

    Initial commit.

---
+++ app.js
@@ -0,0 +1,15 @@
+const startGameBtn = document.getElementById('start-game-btn');
+
+function startGame() {
+  console.log('Game is starting...');
+}
+
+// const person = {
+//   greet: function greet() {
+//     console.log('Hi there');
+//   }
+// }
+
+// person.greet();
+
+startGameBtn.addEventListener('click', startGame);
