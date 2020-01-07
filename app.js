375f1de49661519968d81d9ea1e2a6e42348127d
victorclee <victorchristopherlee@gmail.com>
2020-01-07 14:09:39 -0600

    Added computer choice.

--- app.js
+++ app.js
@@ -1,25 +1,43 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
 const ROCK = 'ROCK';
-const PAPER = "PAPER";
+const PAPER = 'PAPER';
 const SCISSORS = 'SCISSORS';
 const DEFAULT_USER_CHOICE = ROCK;
 
+let gameIsRunning = false;
+
 const getPlayerChoice = function() {
-  const selection = prompt(`${ROCK}, ${PAPER}, or ${SCISSORS}?`, '').toUpperCase();
-  if (
-    selection !== ROCK &&
-    selection !== PAPER &&
-    selection !== SCISSORS
-  ) {
+  const selection = prompt(
+    `${ROCK}, ${PAPER}, or ${SCISSORS}?`,
+    ''
+  ).toUpperCase();
+  if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
     alert(`Invalid choice! We chose ${DEFAULT_USER_CHOICE} for you.`);
     return DEFAULT_USER_CHOICE;
   }
   return selection;
 };
 
+const getComputerChoice = function() {
+  const randomValue = Math.random();
+  if (randomValue < 0.34) {
+    return ROCK;
+  } else if (randomValue < 0.67) {
+    return PAPER;
+  } else {
+    return SCISSORS;
+  }
+};
+
 startGameBtn.addEventListener('click', function() {
+  if (gameIsRunning) {
+    return;
+  }
+  gameIsRunning = true;
   console.log('Game is starting...');
   const playerSelection = getPlayerChoice();
+  const computerChoice = getComputerChoice();
   console.log(playerSelection);
+  console.log(computerChoice);
 });
